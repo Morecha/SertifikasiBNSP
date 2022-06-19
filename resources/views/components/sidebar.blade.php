71675eab4de17b979c1946c580fc124f298beadc
Morecha <ahmad_madda58@yahoo.com>
2022-06-20 04:15:59 +0700

    1

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,39 +7,77 @@
     </a>
     <hr class="sidebar-divider my-0">
 
-    <x-nav-link 
-        text="Dashboard" 
-        icon="tachometer-alt" 
+    <x-nav-link
+        text="Dashboard"
+        icon="tachometer-alt"
         url="{{ route('admin.dashboard') }}"
         active="{{ request()->routeIs('admin.dashboard') ? ' active' : '' }}"
     />
-    
+
+    <hr class="sidebar-divider mb-0">
+
+    @can('artikel')
+    <x-nav-link
+        text="Artikel"
+        icon="arrow-right"
+        url="{{ route('admin.member') }}"
+        active="{{ request()->routeIs('admin.member') ? ' active' : '' }}"
+    />
+    @endcan
+
+    @can('event')
+    <x-nav-link
+        text="Event"
+        icon="flag"
+        url="{{ route('admin.member') }}"
+        active="{{ request()->routeIs('admin.member') ? ' active' : '' }}"
+    />
+    @endcan
+
+    @can('galery-foto')
+    <x-nav-link
+        text="Galery Foto"
+        icon="palette"
+        url="{{ route('admin.member') }}"
+        active="{{ request()->routeIs('admin.member') ? ' active' : '' }}"
+    />
+    @endcan
+
+    @can('klien-kami')
+    <x-nav-link
+        text="Klien Kami"
+        icon="users"
+        url="{{ route('admin.member') }}"
+        active="{{ request()->routeIs('admin.member') ? ' active' : '' }}"
+    />
+    @endcan
+
     <hr class="sidebar-divider mb-0">
 
     @can('member-list')
-    <x-nav-link 
-        text="Member" 
-        icon="users" 
+    <x-nav-link
+        text="Member"
+        icon="users"
         url="{{ route('admin.member') }}"
         active="{{ request()->routeIs('admin.member') ? ' active' : '' }}"
     />
     @endcan
 
     @can('role-list')
-    <x-nav-link 
-        text="Roles" 
-        icon="th-list" 
+    <x-nav-link
+        text="Roles"
+        icon="th-list"
         url="{{ route('admin.roles') }}"
         active="{{ request()->routeIs('admin.roles') ? ' active' : '' }}"
     />
     @endcan
 
     <hr class="sidebar-divider mb-0">
-    
+
     @can('setting-list')
-    <x-nav-link 
-        text="Settings" 
-        icon="cogs" 
+    <x-nav-link
+        text="Settings"
+        icon="cogs"
         url="{{ route('admin.settings') }}"
         active="{{ request()->routeIs('admin.settings') ? ' active' : '' }}"
     />
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{DashboardController, UserController, RoleController};
+use App\Http\Controllers\{DashboardController, UserController, RoleController,
+                            ArtikelController, EventController, GaleryController, KlienController};
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,14 +14,45 @@ Route::group([
 	'as' => 'admin.'
 ], function(){
 	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-	Route::get('/logs', [DashboardController::class, 'activity_logs'])->name('logs');
+    Route::get('/logs', [DashboardController::class, 'activity_logs'])->name('logs');
 	Route::post('/logs/delete', [DashboardController::class, 'delete_logs'])->name('logs.delete');
-	
+
+    // Artikel
+    Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel');
+	Route::get('/artikel/create', [ArtikelController::class, 'create'])->name('artikel.create');
+	Route::post('/artikel/create', [ArtikelController::class, 'store'])->name('artikel.create');
+	Route::get('/artikel/{id}/edit', [ArtikelController::class, 'edit'])->name('artikel.edit');
+	Route::post('/artikel/{id}/update', [ArtikelController::class, 'update'])->name('artikel.update');
+	Route::post('/artikel/{id}/delete', [ArtikelController::class, 'destroy'])->name('artikel.delete');
+
+    // Event
+    Route::get('/event', [EventController::class, 'index'])->name('event');
+	Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
+	Route::post('/event/create', [EventController::class, 'store'])->name('event.create');
+	Route::get('/event/{id}/edit', [EventController::class, 'edit'])->name('event.edit');
+	Route::post('/event/{id}/update', [EventController::class, 'update'])->name('event.update');
+	Route::post('/event/{id}/delete', [EventController::class, 'destroy'])->name('event.delete');
+
+    // Galery
+    Route::get('/galery', [GaleryController::class, 'index'])->name('galery');
+	Route::get('/galery/create', [GaleryController::class, 'create'])->name('galery.create');
+	Route::post('/galery/create', [GaleryController::class, 'store'])->name('galery.create');
+	Route::get('/galery/{id}/edit', [GaleryController::class, 'edit'])->name('galery.edit');
+	Route::post('/galery/{id}/update', [GaleryController::class, 'update'])->name('galery.update');
+	Route::post('/galery/{id}/delete', [GaleryController::class, 'destroy'])->name('galery.delete');
+
+    // Klien
+    Route::get('/klien', [KlienController::class, 'index'])->name('klien');
+	Route::get('/klien/create', [KlienController::class, 'create'])->name('klien.create');
+	Route::post('/klien/create', [KlienController::class, 'store'])->name('klien.create');
+	Route::get('/klien/{id}/edit', [KlienController::class, 'edit'])->name('klien.edit');
+	Route::post('/klien/{id}/update', [KlienController::class, 'update'])->name('klien.update');
+	Route::post('/klien/{id}/delete', [KlienController::class, 'destroy'])->name('klien.delete');
+
 	// Settings menu
 	Route::view('/settings', 'admin.settings')->name('settings');
 	Route::post('/settings', [DashboardController::class, 'settings_store'])->name('settings');
-	
+
 	// Profile menu
 	Route::view('/profile', 'admin.profile')->name('profile');
 	Route::post('/profile', [DashboardController::class, 'profile_update'])->name('profile');
